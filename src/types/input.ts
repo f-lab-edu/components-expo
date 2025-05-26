536742224de1b55a5cba4a39969e9a13143032b3
lecto17 <khwoo_rpsit@naver.com>
2025-05-26 21:34:37 +0900

    chore: input Props type 추가

--- src/types/input.ts
+++ src/types/input.ts
@@ -1,5 +1,17 @@
+import type { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+
 export type InputProps = {
   required?: boolean;
   disabled?: boolean;
   labelText?: string;
+  registerKey?: string;
+  register?: UseFormRegister<any>;
+  requiredPattern?: {
+    required: string;
+    pattern: {
+      value: any;
+      message: string;
+    };
+  };
+  errors?: FieldErrors<FieldValues>;
 };
