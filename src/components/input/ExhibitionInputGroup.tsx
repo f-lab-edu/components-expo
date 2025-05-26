0474924dec78bc0e791e4d8ccef7f124b3cbb656
lecto17 <khwoo_rpsit@naver.com>
2025-05-26 21:28:35 +0900

    chore: Input 컴포넌트 내 react-hook-form 적용

--- src/components/input/ExhibitionInputGroup.tsx
+++ src/components/input/ExhibitionInputGroup.tsx
@@ -1,17 +1,42 @@
-import type { MouseEvent } from 'react';
+import { useState, type MouseEvent } from 'react';
 import Input from './Input';
+import { useForm } from 'react-hook-form';
+// import { EMAIL_INPUT, PHONE_INPUT } from '@/constants/input';
+import { EMAIL_INPUT, PHONE_INPUT } from '../../constants/input';
 
 export default function ExhibitionInputGroup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [data, setData] = useState('');
+
   const handleClickSubmit = (e: MouseEvent) => {
     e.stopPropagation();
   };
 
   return (
     <div className="w-full h-full flex justify-center items-center relative">
-      <form action="">
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
         <div className="group flex flex-col p-2 gap-2">
-          <Input labelText="이름" />
-          <Input labelText="연락처" required />
+          <Input register={register} registerKey="name" labelText="이름" />
+          <Input
+            register={register}
+            registerKey="phone"
+            labelText="연락처"
+            required
+            requiredPattern={PHONE_INPUT}
+            errors={errors}
+          />
+          <Input
+            register={register}
+            registerKey="email"
+            labelText="이메일"
+            required
+            requiredPattern={EMAIL_INPUT}
+            errors={errors}
+          />
           <Input labelText="disabled" disabled />
         </div>
 
@@ -21,6 +46,7 @@ export default function ExhibitionInputGroup() {
         >
           submit
         </button>
+        {data}
       </form>
     </div>
   );
