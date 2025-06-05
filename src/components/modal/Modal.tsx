5590307334778b547c08a889a91ad6120cb68f1b
lecto17 <khwoo_rpsit@naver.com>
2025-06-05 19:05:40 +0900

    chore: local modal로 출력되도록 작업

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -4,9 +4,10 @@ import { createPortal } from 'react-dom';
 type ModalProps = {
   onClose: Dispatch<SetStateAction<boolean>>;
   children: React.ReactNode;
+  domNode?: Element;
 };
 
-export default function Modal({ children, onClose }: ModalProps) {
+export default function Modal({ children, onClose, domNode }: ModalProps) {
   const handleClickOutSide = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
       onClose((prev) => !prev);
@@ -14,16 +15,18 @@ export default function Modal({ children, onClose }: ModalProps) {
   };
   return createPortal(
     <section
-      className="modal-overlay w-full h-full flex justify-center items-center bg-black/20 fixed top-0 left-0 z-20"
+      className={`modal-overlay w-full h-full flex justify-center items-center bg-black/20 top-0 left-0 z-20 ${
+        domNode ? 'absolute' : 'fixed'
+      }`}
       onClick={handleClickOutSide}
     >
-      <div className="flex justify-center items-center w-[500px] h-[300px] p-4 bg-white relative rounded-md">
-        <button className="absolute right-4 top-4" onClick={() => onClose((prev) => !prev)}>
+      <div className="flex justify-center items-center w-[40%] h-[40%] p-4 bg-white relative rounded-md">
+        <button className="absolute right-2 top-2" onClick={() => onClose((prev) => !prev)}>
           X
         </button>
         {children}
       </div>
     </section>,
-    document.body
+    domNode || document.body
   );
 }
