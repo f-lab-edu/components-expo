c6a1dca3ac064436518c79238f5f20c47b42097e
lecto17 <khwoo_rpsit@naver.com>
2025-06-09 15:22:07 +0900

    feat: modal 컴포넌트 추가

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,36 @@
+import type { Dispatch, SetStateAction } from 'react';
+import { createPortal } from 'react-dom';
+import CloseIcon from '@/assets/close.svg';
+
+type ModalProps = {
+  onClose: Dispatch<SetStateAction<boolean>>;
+  children: React.ReactNode;
+  domNode?: Element;
+};
+
+export default function Modal({ children, onClose, domNode }: ModalProps) {
+  const handleClickOutSide = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      onClose((prev) => !prev);
+    }
+  };
+  return createPortal(
+    <section
+      className={`modal-overlay w-full h-full flex justify-center items-center bg-black/20 top-0 left-0 z-20 ${
+        domNode ? 'absolute' : 'fixed'
+      }`}
+      onClick={handleClickOutSide}
+    >
+      <div className="flex justify-center items-center w-[40%] h-[40%] p-4 bg-white relative rounded-md">
+        <img
+          src={CloseIcon}
+          className="absolute right-3 top-3"
+          onClick={() => onClose((prev) => !prev)}
+          alt="close icon"
+        />
+        {children}
+      </div>
+    </section>,
+    domNode || document.body
+  );
+}
