8ae4f62d237d29be168002da61dc8fd5f3546985
lecto17 <khwoo_rpsit@naver.com>
2025-06-09 15:45:45 +0900

    chore: modal close 버튼 css fade 효과 적용

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import type { Dispatch, SetStateAction } from 'react';
+import { useState, type Dispatch, type SetStateAction } from 'react';
 import { createPortal } from 'react-dom';
 import CloseIcon from '@/assets/close.svg';
 
@@ -9,11 +9,24 @@ type ModalProps = {
 };
 
 export default function Modal({ children, onClose, domNode }: ModalProps) {
+  const [isActive, setIsActive] = useState(false);
+
   const handleClickOutSide = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
       onClose((prev) => !prev);
     }
   };
+
+  const handleEnter = () => {
+    setIsActive(true);
+  };
+
+  const handleLeave = () => {
+    setTimeout(() => {
+      setIsActive(false);
+    }, 300);
+  };
+
   return createPortal(
     <section
       className={`modal-overlay w-full h-full flex justify-center items-center bg-black/20 top-0 left-0 z-20 ${
@@ -22,12 +35,15 @@ export default function Modal({ children, onClose, domNode }: ModalProps) {
       onClick={handleClickOutSide}
     >
       <div className="flex justify-center items-center w-[40%] h-[40%] p-4 bg-white relative rounded-md">
-        <img
-          src={CloseIcon}
-          className="absolute right-3 top-3"
-          onClick={() => onClose((prev) => !prev)}
-          alt="close icon"
-        />
+        <div
+          className={`absolute right-3 top-3 border border-transparent rounded-full p-2 cursor-pointer transition ${
+            isActive ? 'bg-gray-200' : ''
+          }`}
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        >
+          <img src={CloseIcon} onClick={() => onClose((prev) => !prev)} alt="close icon" />
+        </div>
         {children}
       </div>
     </section>,
