1f4f1802c16e614f9f8923a0ee4c83081b8e8f43
lecto17 <khwoo_rpsit@naver.com>
2025-06-12 23:46:49 +0900

    feat: searchbar 컴포넌트 기본 기능 구현

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import { useSearchBar } from '@/components/searchbar/hooks/useSearchBar';
+import React, {
+  isValidElement,
+  type Dispatch,
+  type ReactElement,
+  type SetStateAction,
+} from 'react';
 
 type SearchBarProps = {
   children: React.ReactNode;
 };
 
+type SearchBarElementProps = {
+  index?: number;
+  isFirstChild: boolean;
+  isActiveMenu: boolean;
+  setActiveMenu: Dispatch<SetStateAction<number>>;
+};
+
+const gridClass = (count: number) => {
+  switch (count) {
+    case 1:
+      return 'grid-cols-1';
+    case 2:
+      return 'grid-cols-2';
+    case 3:
+      return 'grid-cols-3';
+    case 4:
+      return 'grid-cols-4';
+    default:
+      return '';
+  }
+};
+
 export default function SearchBar({ children }: SearchBarProps) {
   const childrenCount = React.Children.count(children);
+  const { activeMenu, setActiveMenu } = useSearchBar();
 
   return (
     <div
-      className={`w-[60%] grid gap-2 grid-cols-${childrenCount} px-4 py-2 box-border border border-gray-300 bg-whiite shadow-lg rounded-4xl`}
+      className={`w-[60%] grid gap-2 ${gridClass(
+        childrenCount
+      )} box-border border border-gray-300 shadow-lg rounded-4xl overflow-hidden ${
+        activeMenu !== -1 ? 'bg-[#ebebeb]' : ''
+      }`}
     >
-      {children}
+      {React.Children.map(children, (child, idx) => {
+        if (isValidElement(child)) {
+          return React.cloneElement(child as ReactElement<SearchBarElementProps>, {
+            index: idx,
+            isFirstChild: idx === 0,
+            isActiveMenu: activeMenu === idx,
+            setActiveMenu,
+          });
+        }
+        return child;
+      })}
     </div>
   );
 }
