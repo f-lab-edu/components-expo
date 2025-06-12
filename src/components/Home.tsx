f1a76e0085df0a163ddd011226aeef6e4f03b5fa
lecto17 <khwoo_rpsit@naver.com>
2025-06-12 23:52:18 +0900

    chore: searchbar 내 렌더될 컴포넌트 추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,17 @@
 import SearchBar from '@/components/searchbar/SearchBar';
-import ExhibitionButton from './button/ExhibitionButton';
-import Layout from './common/Layout';
-import ExhibitionInput from './input/ExhibitionInput';
-import ExhibitionInputGroup from './input/ExhibitionInputGroup';
+import SearchBarElement from '@/components/searchbar/components/SearchBarElement';
 
 export default function Home() {
-  // const components = [
-  //   <ExhibitionButton />,
-  //   <ExhibitionInput />,
-  //   <ExhibitionInputGroup />,
-  //   4,
-  //   5,
-  //   6,
-  //   7,
-  //   8,
-  //   9,
-  // ];
   return (
     <main className="w-full h-full flex justify-center items-center">
       <SearchBar>
-        <div>test</div>
-        <div>test1</div>
-        <div>test2</div>
-        <div>test3</div>
+        <SearchBarElement
+          title="여행지"
+          content={<input className=" outline-none" type="text" placeholder="여행지 검색" />}
+        />
+        <SearchBarElement title="체크인" content="날짜 추가" />
+        <SearchBarElement title="체크아웃" content="날짜 추가" />
+        <SearchBarElement title="여행자" content="게스트 추가" />
       </SearchBar>
     </main>
   );
