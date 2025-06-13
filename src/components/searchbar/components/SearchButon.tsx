5b4504f21bd61e67ee29b816c661a77e7c485b51
lecto17 <khwoo_rpsit@naver.com>
2025-06-13 16:57:37 +0900

    chore: 검색 버튼 컴포넌트 추가

--- src/components/searchbar/components/SearchButon.tsx
+++ src/components/searchbar/components/SearchButon.tsx
@@ -1,7 +1,25 @@
+import search from '@/components/searchbar/assets/search.svg';
+import { useSearchBar } from '@/components/searchbar/hooks/useSearchBar';
+
 export default function SearchButon() {
+  const { activeMenu } = useSearchBar();
+
+  const isMenuActive = activeMenu !== -1;
+
   return (
-    <div>
-      <button></button>
+    <div
+      className={`flex space-x-1 items-center h-4 ${
+        isMenuActive ? 'w-12' : 'w-4'
+      } transition-all duration-500`}
+    >
+      <img className="w-4 h-4" src={search} alt="search image" />
+      <span
+        className={`text-sm min-w-fit whitespace-nowrap font-semibold text-white duration-200 opacity-0 invisible ${
+          isMenuActive ? 'visible opacity-100' : ''
+        }`}
+      >
+        검색
+      </span>
     </div>
   );
 }
