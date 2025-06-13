1e8063ecb5c678d8eec98e76a99f60c5213dc0ab
lecto17 <khwoo_rpsit@naver.com>
2025-06-13 16:56:45 +0900

    chore: searchbar 외부 클릭 시 activemenu 초기화하는 훅 연동

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,6 +1,8 @@
+import useOutsideClick from '@/components/searchbar/hooks/useOutsideClick';
 import { useSearchBar } from '@/components/searchbar/hooks/useSearchBar';
 import React, {
   isValidElement,
+  useRef,
   type Dispatch,
   type ReactElement,
   type SetStateAction,
@@ -35,10 +37,14 @@ const gridClass = (count: number) => {
 export default function SearchBar({ children }: SearchBarProps) {
   const childrenCount = React.Children.count(children);
   const { activeMenu, setActiveMenu } = useSearchBar();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick(wrapperRef, () => setActiveMenu(-1));
 
   return (
     <div
-      className={`w-[60%] grid gap-2 ${gridClass(
+      ref={wrapperRef}
+      className={`w-[60%] grid gap-2 relative ${gridClass(
         childrenCount
       )} box-border border border-gray-300 shadow-lg rounded-4xl overflow-hidden ${
         activeMenu !== -1 ? 'bg-[#ebebeb]' : ''
