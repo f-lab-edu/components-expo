ed34fef361d854e863f796d62a96f42d735e4663
lecto17 <khwoo_rpsit@naver.com>
2025-06-14 15:49:23 +0900

    chore: selectbox 컴포넌트 내부에 렌더될 컴포넌트 주입받도록 변경

--- src/components/selectbox/Selectbox.tsx
+++ src/components/selectbox/Selectbox.tsx
@@ -1,46 +1,38 @@
-import type { RecommendPlace } from '@/components/selectbox/types/type';
+import useOutsideClick from '@/components/selectbox/hooks/useOutsideClick';
+import { useRef, type SetStateAction } from 'react';
 
 type SelectboxProps = {
-  data: RecommendPlace[];
+  children: React.ReactNode;
+  isCloseOnClickOutside?: boolean;
+  isCloseOnPressedESC?: boolean;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Selectbox({ data }: SelectboxProps) {
-  return (
-    <div className="flex flex-col py-3 pl-3 pr-1 rounded-3xl h-[540px] overflow-hidden shadow-lg">
-      {data.map((group, idx) => {
-        const { title, items } = group;
+export default function Selectbox({
+  children,
+  isCloseOnClickOutside = true,
+  isCloseOnPressedESC = true,
+  isOpen,
+  setIsOpen,
+}: SelectboxProps) {
+  const ref = useRef<HTMLDivElement>(null);
 
-        return (
-          <div className="flex flex-col py-6 px-3 overflow-y-scroll">
-            <span className="text-sm mb-2">{title}</span>
-            <ul key={idx} className="flex flex-col space-y-2">
-              {items.map((item) => {
-                const { itemId, itemTitle, image, description } = item;
-                return (
-                  <li
-                    key={itemId}
-                    value={itemId}
-                    className="flex items-center w-full p-2 hover:bg-[#ebebeb] rounded-2xl cursor-pointer"
-                  >
-                    <img
-                      src={
-                        image ||
-                        'https://thumb.ac-illust.com/b1/b170870007dfa419295d949814474ab2_t.jpeg'
-                      }
-                      alt={`${itemTitle} image`}
-                      className="w-12 object-cover mr-2"
-                    />
-                    <p className="flex flex-col ml-2">
-                      <span>{itemTitle}</span>
-                      <span>{description}</span>
-                    </p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
-    </div>
+  useOutsideClick({
+    ref,
+    isCloseOnClickOutside,
+    isCloseOnPressedESC,
+    handler: () => setIsOpen(false),
+  });
+
+  return (
+    isOpen && (
+      <div
+        className="flex flex-col py-3 pl-3 pr-1 rounded-3xl h-[540px] overflow-hidden shadow-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white"
+        ref={ref}
+      >
+        {children}
+      </div>
+    )
   );
 }
