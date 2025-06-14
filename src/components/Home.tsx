5f14a59569d5754e987af0db182054395111e255
lecto17 <khwoo_rpsit@naver.com>
2025-06-14 16:58:59 +0900

    chore: selectbox 외부 클릭 시 닫히지 말아야할 ref 추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import RecommendPlaceList from '@/components/selectbox/RecommendPlaceList';
 import Selectbox from '@/components/selectbox/Selectbox';
 import type { RecommendPlaceResponse } from '@/components/selectbox/types/type';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   /**
@@ -16,8 +16,13 @@ export default function Home() {
    * 2. 사용자가 검색한 목록
    */
 
+  const ref = useRef<HTMLSpanElement | null>(null);
   const [data, setData] = useState<RecommendPlaceResponse[]>([]);
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickSelectbox = useCallback(() => {
+    setIsOpen(true);
+  }, []);
 
   useEffect(() => {
     import('@/components/selectbox/mocks/data.json').then((res) => setData(res.default));
@@ -25,10 +30,14 @@ export default function Home() {
 
   return (
     <main className="w-full h-full flex items-center justify-center relative">
-      <span className="w-24 border p-4 rounded-lg cursor-pointer" onClick={() => setIsOpen(true)}>
+      <span
+        className="w-24 border p-4 rounded-lg cursor-pointer"
+        onClick={handleClickSelectbox}
+        ref={ref}
+      >
         임시 selectbox
       </span>
-      <Selectbox isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Selectbox isOpen={isOpen} setIsOpen={setIsOpen} exceptRef={ref}>
         <RecommendPlaceList data={data} />
       </Selectbox>
     </main>
