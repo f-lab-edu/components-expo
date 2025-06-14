8dea29462790a33ecf3581928d7d58a635144a31
lecto17 <khwoo_rpsit@naver.com>
2025-06-14 15:52:09 +0900

    chore: 홈화면 내 selectbox 컴포넌트 출력

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
+import RecommendPlaceList from '@/components/selectbox/RecommendPlaceList';
 import Selectbox from '@/components/selectbox/Selectbox';
-import type { RecommendPlace } from '@/components/selectbox/types/type';
+import type { RecommendPlaceResponse } from '@/components/selectbox/types/type';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -9,21 +10,27 @@ export default function Home() {
    * 1. api로 받아온 데이터(mocks 폴더에서 import)
    *   - data: [{
    *       title: string,
-   *       items: RecommendPlace[]
+   *       items: RecommendPlaceResponse[]
    *    }]
    * TODO
    * 2. 사용자가 검색한 목록
    */
 
-  const [data, setData] = useState<RecommendPlace[]>([]);
+  const [data, setData] = useState<RecommendPlaceResponse[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     import('@/components/selectbox/mocks/data.json').then((res) => setData(res.default));
   }, []);
 
   return (
-    <main className="w-full h-full flex items-center justify-center">
-      <Selectbox data={data} />
+    <main className="w-full h-full flex items-center justify-center relative">
+      <span className="w-24 border p-4 rounded-lg cursor-pointer" onClick={() => setIsOpen(true)}>
+        임시 selectbox
+      </span>
+      <Selectbox isOpen={isOpen} setIsOpen={setIsOpen}>
+        <RecommendPlaceList data={data} />
+      </Selectbox>
     </main>
   );
 }
