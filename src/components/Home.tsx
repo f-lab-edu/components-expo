842ec10c3504e1fab69332ba5a73572da5c543d9
lecto17 <khwoo_rpsit@naver.com>
2025-06-17 22:53:25 +0900

    chore: Searchbar component interface 전달

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,11 +3,12 @@ import SearchBar from '@/components/searchbar/SearchBar';
 import Carousel from '@/components/carousel/Carousel';
 import RankingMovie from '@/components/movie/RankingMovie';
 import { movies } from '@/mock/data';
+import { searchbarElements } from '@/components/searchbar/mocks/searchbar';
 
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col justify-center items-center space-y-32">
-      <SearchBar />
+      <SearchBar elements={searchbarElements} />
 
       <div className="w-[1400px]">
         <Carousel>
