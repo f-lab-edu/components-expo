d1afb7b839b909d6a7b0e533f7c1a8d255b037df
lecto17 <khwoo_rpsit@naver.com>
2025-06-18 17:02:18 +0900

    chore: react-query 라이브러리 추가 및 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './global.css';
 import Home from './components/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Home />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
