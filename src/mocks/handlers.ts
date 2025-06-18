6db766b08bf5dfde578cad2579873150440f124d
lecto17 <khwoo_rpsit@naver.com>
2025-06-18 17:08:56 +0900

    chore: mock data msw로 제공

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,17 @@
-import { http } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('/api/user', ({ request }) => {
-    console.log('[MSW] Req: ', request);
-    return new Response();
+  http.get('/api/lodgings', async () => {
+    const data = (await import('@/mocks/lodgings.json')).default;
+    console.log('[MSW]: ', data);
+
+    return HttpResponse.json({ data, statusCode: 200, message: 'success' });
+  }),
+
+  http.get('/api/movies', async () => {
+    const data = (await import('@/mocks/movies.json')).default;
+    console.log('[MSW]: ', data);
+
+    return HttpResponse.json(data);
   }),
 ];
