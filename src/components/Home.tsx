2f09c57f1a254d14a0c770e575013c68510c5713
lecto17 <khwoo_rpsit@naver.com>
2025-06-18 17:08:22 +0900

    chore: 홈 화면 내 숙박 데이터 연동

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,30 @@
 import SearchBar from '@/components/searchbar/SearchBar';
 
 import Carousel from '@/components/carousel/Carousel';
-import RankingMovie from '@/components/movie/RankingMovie';
-import { movies } from '@/mock/data';
 import { searchbarElements } from '@/components/searchbar/mocks/searchbar';
+import { useGetLodgingList } from '@/components/lodging/hooks/useGetLodgingList';
+import type { LodgingProps } from '@/components/lodging/types/lodging';
+import Lodging from '@/components/lodging/Lodging';
 
 export default function Home() {
+  const { lodging, error, isLoading } = useGetLodgingList<LodgingProps>();
+
+  if (isLoading) return <>data is loading...</>;
+  if (error) return <>Something Error happen</>;
+
   return (
     <main className="w-full h-full flex flex-col justify-center items-center space-y-32">
       <SearchBar elements={searchbarElements} />
 
       <div className="w-[1400px]">
         <Carousel>
-          {movies.map(({ thumbnail, ranking, description, categories }) => (
-            <li key={ranking}>
-              {
-                <RankingMovie
-                  thumbnail={thumbnail}
-                  ranking={ranking}
-                  description={description}
-                  categories={categories}
-                />
-              }
-            </li>
-          ))}
+          {lodging.map((el) => {
+            return (
+              <li key={el.id}>
+                <Lodging {...el} />
+              </li>
+            );
+          })}
         </Carousel>
       </div>
     </main>
