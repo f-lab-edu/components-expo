5c007dbdd7134f55c0be3a919f36dc6fe68a67d3
lecto17 <khwoo_rpsit@naver.com>
2025-06-19 16:10:02 +0900

    chore: msw put handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,26 @@
 import { http, HttpResponse } from 'msw';
 
+type UpdateRequest = {
+  id: string;
+};
+
 export const handlers = [
   http.get('/api/lodgings', async () => {
     const data = (await import('@/mocks/lodgings.json')).default;
-    console.log('[MSW]: ', data);
-
     return HttpResponse.json({ data, statusCode: 200, message: 'success' });
+  }),
+
+  http.put('/api/lodgings', async ({ request }) => {
+    let mockLodgings = await import('@/mocks/lodgings.json').then((m) => m.default);
+
+    const body = await request.json();
+    const id = (body as UpdateRequest).id;
+
+    mockLodgings = mockLodgings.map((item) =>
+      item.id === id ? { ...item, isLiked: !item.isLiked } : item
+    );
+
+    return HttpResponse.json({ data: mockLodgings, statusCode: 200, message: 'success' });
   }),
 
   http.get('/api/movies', async () => {
