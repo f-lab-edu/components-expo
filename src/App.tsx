8140df6b8c7b0c9ccf3494c72d39f4395bd9c170
lecto17 <khwoo_rpsit@naver.com>
2025-06-21 14:57:18 +0900

    chore: react-query 설치 및 query client 주입

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './global.css';
 import Home from './components/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  });
   return (
     <>
-      <Home />
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
     </>
   );
 }
