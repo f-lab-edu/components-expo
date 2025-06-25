c5aca0f815e5da68105c67ee10ee34b823dc2d0e
lecto17 <khwoo_rpsit@naver.com>
2025-06-25 19:06:11 +0900

    chore: skeleton item 갯수 구하는 로직 변경

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,15 +10,13 @@ import { useGetLodgingList } from '@/components/lodging/hooks/useGetLodgingList'
 import type { LodgingProps } from '@/components/lodging/types/lodging';
 import Lodging from '@/components/lodging/Lodging';
 import SkeletonLodging from '@/components/lodging/SkeletonLodging';
-import { LODGING_WIDTH } from '@/components/lodging/constants/constant';
-import { useSkeletonUI } from '@/components/lodging/hooks/useSkeletonUI';
 import { useUpdateLodgingLike } from '@/components/lodging/hooks/useUpdateLodgingLike';
+import { GET_LODGING_COUNT } from '@/components/lodging/constants/constants';
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
-  const { lodgings, error, isLoading } = useGetLodgingList<LodgingProps>();
+  const { lodgings, error, isLoading } = useGetLodgingList<LodgingProps[]>();
   const { mutate: updateLikeLodging } = useUpdateLodgingLike();
-  const { skeletonCount } = useSkeletonUI({ containerRef, itemWidth: LODGING_WIDTH });
 
   const ref = useRef<HTMLSpanElement | null>(null);
   const [data, setData] = useState<RecommendPlaceResponse[]>([]);
@@ -68,7 +66,7 @@ export default function Home() {
       <div className="w-[1400px] space-y-6" ref={containerRef}>
         <Carousel>
           {isLoading
-            ? Array.from({ length: skeletonCount }).map(() => <SkeletonLodging />)
+            ? Array.from({ length: GET_LODGING_COUNT }).map(() => <SkeletonLodging />)
             : lodgings.map((el) => {
                 return (
                   <li key={el.id}>
---
+++ src/components/lodging/constants/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * lodgings api call 할 때 사용할 size
+ *  */
+export const GET_LODGING_COUNT = 10;
